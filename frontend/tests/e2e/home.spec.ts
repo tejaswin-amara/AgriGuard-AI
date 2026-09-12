fa5ae516eacb7ac5e1cb0ff60482544efdf34d73
tejaswin-amara <97726757+tejaswin-amara@users.noreply.github.com>
2026-09-12 08:20:23 +0000

    chore: setup project structure, formatters, and initial templates

--- frontend/tests/e2e/home.spec.ts
+++ frontend/tests/e2e/home.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test";
+import { expect, test } from "@playwright/test";
 
 test("has title and main heading", async ({ page }) => {
 	await page.goto("/");
